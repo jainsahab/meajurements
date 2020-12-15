964d7b738286f0fc21b7dcb589b2061fe64a6324
Prateek Jain <prateek.jain23995@yahoo.com>
2020-12-15 11:06:46 +0800

    Adding support to extract custom tags in ring handler

--- test/meajurements/ring_test.clj
+++ test/meajurements/ring_test.clj
@@ -37,3 +37,33 @@
                  :status   200
                  :app-name "foo-app-name"}]
                @increment-args))))))
+
+(deftest statsd-reporting-custom-tags-test
+  (testing "when the handler is instrumented with custom tags extractor function"
+    (testing "timing is called with custom and default tags"
+      (let [timing-args (atom nil)
+            wrapped-handler (-> (constantly {:status 200 :body "ok"})
+                                (ring/wrap-statsd-reporting "foo-app-name" "foo-api-name" (fn [req _]
+                                                                                            (select-keys req [:country-id]))))]
+        (with-redefs [core/timing (fn [& args]
+                                    (reset! timing-args (vec args)))]
+          (wrapped-handler {:country-id "SG"}))
+        (is (= {:api-name   "foo-api-name"
+                :status     200
+                :app-name   "foo-app-name"
+                :country-id "SG"}
+               (nth @timing-args 2)))))
+
+    (testing "increment is called with the custom and default tags"
+      (let [increment-args (atom nil)
+            wrapped-handler (-> (constantly {:status 200 :body "ok"})
+                                (ring/wrap-statsd-reporting "foo-app-name" "foo-api-name" (fn [req _]
+                                                                                            (select-keys req [:country-id]))))]
+        (with-redefs [core/increment (fn [& args]
+                                       (reset! increment-args (vec args)))]
+          (wrapped-handler {:country-id "SG"}))
+        (is (= {:api-name   "foo-api-name"
+                :status     200
+                :app-name   "foo-app-name"
+                :country-id "SG"}
+               (nth @increment-args 1)))))))
